f36c1a55b35767302b43262ae994be7e2a29144f
Gugle <administration@ztxy666.cn>
2026-01-20 04:55:07 +0800

    feat(mixin): 添加标题屏幕混入增强联机允许
    - 将 ItemMixin 移至 client 端配置，添加 TitleScreenMixin 以修改标题屏幕行为
    - 在 createNormalMenuOptions 方法中注入代码，确保允许多人联机选项始终开启
    - 更新多个版本的 anisum.mixins.json 配置，调整 ItemMixin 的位置以符合不同平台需求

---
+++ src/main/java/dev/anvilcraft/resource/anisum/mixin/TitleScreenMixin.java
@@ -0,0 +1,26 @@
+package dev.anvilcraft.resource.anisum.mixin;
+
+import com.llamalad7.mixinextras.expression.Definition;
+import com.llamalad7.mixinextras.expression.Expression;
+import com.llamalad7.mixinextras.sugar.Local;
+import com.llamalad7.mixinextras.sugar.ref.LocalBooleanRef;
+import net.minecraft.client.gui.screens.TitleScreen;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import javax.annotation.Nonnull;
+
+@Mixin(TitleScreen.class)
+public class TitleScreenMixin {
+    @Definition(id = "allowsMultiplayer", method = "Lnet/minecraft/client/Minecraft;allowsMultiplayer()Z")
+    @Expression("? = ?.allowsMultiplayer()")
+    @Inject(
+        method = "createNormalMenuOptions",
+        at = @At(value = "MIXINEXTRAS:EXPRESSION", shift = At.Shift.AFTER)
+    )
+    void createNormalMenuOptions(int i, int j, CallbackInfo ci, @Local(name = "bl") @Nonnull LocalBooleanRef bl) {
+        bl.set(true);
+    }
+}
