33e36734e5e00e7a0e8f5a8f868c05f11b7d3e1b
QiuShui1012 <150409561+QiuShui1012@users.noreply.github.com>
2026-01-22 07:06:55 +0800

    fix(run): 修复Forge无法启动的问题

--- src/main/java/dev/anvilcraft/resource/anisum/mixin/LootPoolAccessor.java
+++ src/main/java/dev/anvilcraft/resource/anisum/mixin/LootPoolAccessor.java
@@ -5,8 +5,16 @@ import net.minecraft.world.level.storage.loot.entries.LootPoolEntryContainer;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.gen.Accessor;
 
+//#if MC==11605 && FORGE
+//$$ import java.util.List;
+//#endif
+
 @Mixin(LootPool.class)
 public interface LootPoolAccessor {
     @Accessor
+    //#if MC==11605 && FORGE
+    //$$ List<LootPoolEntryContainer> getEntries();
+    //#else
     LootPoolEntryContainer[] getEntries();
+    //#endif
 }
--- src/main/java/dev/anvilcraft/resource/anisum/mixin/LootTableAccessor.java
+++ src/main/java/dev/anvilcraft/resource/anisum/mixin/LootTableAccessor.java
@@ -5,8 +5,16 @@ import net.minecraft.world.level.storage.loot.LootTable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.gen.Accessor;
 
+//#if FORGE
+//$$ import java.util.List;
+//#endif
+
 @Mixin(LootTable.class)
 public interface LootTableAccessor {
     @Accessor
+    //#if FORGE
+    //$$ List<LootPool> getPools();
+    //#else
     LootPool[] getPools();
+    //#endif
 }
